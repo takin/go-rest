d1eb5ffed369f08a8931c17c2ec17decd3b1d496
Luqmanul Hakim <arlhba@gmail.com>
2019-02-18 14:54:27 +0700

    tambahan setstatus header karena sebelumnya semua request status nya 200

--- nst/controller.go
+++ nst/controller.go
@@ -13,6 +13,7 @@ type Response struct {
 	Status  int         `json:"status"`
 	Message string      `json:"message"`
 	Payload interface{} `json:"payload"`
+	Errors  interface{} `json:"errors,omitempty"`
 }
 
 // Controller main controller structre
@@ -29,6 +30,10 @@ func (c *Controller) Serve() {
 		if c.Res.Status == 0 {
 			c.Res.Status = 403
 		}
+
+		// Set Http Response code
+		c.Ctx.Output.SetStatus(c.Res.Status)
+
 		c.Res.Message = c.Err.Error()
 	} else {
 		c.Res.Message = "Success"
