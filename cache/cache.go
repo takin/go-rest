f4406bdbf1246ff07f5780ce91f12d59bd66fa74
Syamsul Muttaqin <lombok.oc@gmail.com>
2019-03-18 02:19:54 +0700

    fixing bugs on write function

--- cache/cache.go
+++ cache/cache.go
@@ -8,6 +8,16 @@ import (
 	jsoniter "github.com/json-iterator/go"
 )
 
+type Channel struct {
+	Data   chan interface{}
+	Error  chan error
+	IsOpen chan bool
+}
+
+type ChannelHandler interface {
+	Close()
+}
+
 type FileCache struct {
 	Path      string
 	Overwrite bool
@@ -16,6 +26,12 @@ type FileCache struct {
 type CacheHandler interface {
 	Write(interface{}) (bool, error)
 	Read() (interface{}, error)
+}
+
+func (c *Channel) Close() {
+	close(c.Data)
+	close(c.Error)
+	close(c.IsOpen)
 }
 
 func (c *FileCache) Read() (content interface{}, err error) {
@@ -30,14 +46,26 @@ func (c *FileCache) Read() (content interface{}, err error) {
 }
 
 func (c *FileCache) Write(content interface{}) (ok bool, err error) {
-	file, err := os.Open(c.Path)
-	defer file.Close()
-	if (err != nil) || (err == nil && c.Overwrite) {
-		if cache, err := jsoniter.Marshal(content); err != nil {
-			file.Write(cache)
-			return true, nil
-		}
-		return false, err
+	var cache []byte
+	var file *os.File
+	ok = false
+	if cache, err = jsoniter.Marshal(content); err != nil {
+		return
 	}
-	return false, fmt.Errorf("File Already Exixted")
+	file, err = os.Open(c.Path)
+	defer file.Close()
+	if err == nil && c.Overwrite {
+		if _, err = file.Write(cache); err == nil {
+			ok = true
+		}
+		return
+	}
+	if file, err = os.Create(c.Path); err == nil {
+		if _, err = file.Write(cache); err == nil {
+			ok = true
+		}
+		return
+	}
+	err = fmt.Errorf("File %s already exist in the path", c.Path)
+	return
 }
